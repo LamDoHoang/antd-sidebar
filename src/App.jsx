1f7ca48aa4b3532adfc7f2f679089e6faf4a81da
LamDoHoang <irokatsuna@gmail.com>
2024-12-12 19:44:30 +0700

    add drawer and custom it

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,71 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { Button, Drawer, Input, Space } from "antd";
+import { CloseOutlined, SearchOutlined } from "@ant-design/icons";
+import { AnimatePresence, motion } from "motion/react";
+const App = () => {
+  const [open, setOpen] = useState(false);
+  const [openSearch, setOpenSearch] = useState(false);
+  const showDrawer = () => {
+    setOpen(true);
+  };
+  const onClose = () => {
+    setOpen(false);
+  };
 
-function App() {
-  const [count, setCount] = useState(0)
-
+  const onSearch = () => {
+    setOpenSearch(!openSearch);
+  };
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Space>
+        <Button type="primary" onClick={showDrawer}>
+          Open
+        </Button>
+      </Space>
+      <Drawer
+        title={
+          <AnimatePresence mode="wait">
+            {!openSearch ? (
+              <motion.div
+                key="modal"
+                initial={{ scale: 0 }}
+                animate={{ scale: 1 }}
+                // exit={{ scale: 0 }}
+              >
+                <text>ATN</text>
+              </motion.div>
+            ) : (
+              <motion.div
+                key="search"
+                initial={{ scale: 0 }}
+                animate={{ scale: 1 }}
+                // exit={{ scale: 0 }}
+              >
+                <Input
+                  addonBefore={<SearchOutlined />}
+                  placeholder="large size"
+                />
+              </motion.div>
+            )}
+          </AnimatePresence>
+        }
+        placement="left"
+        width={500}
+        onClose={onClose}
+        open={open}
+        extra={
+          <Space style={{ marginLeft: 10 }}>
+            <Button type="primary" onClick={onSearch}>
+              {!openSearch ? <SearchOutlined /> : <CloseOutlined />}
+            </Button>
+          </Space>
+        }
+      >
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+      </Drawer>
     </>
-  )
-}
-
-export default App
+  );
+};
+export default App;
